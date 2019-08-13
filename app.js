9d57390683f9a3aa182e6c546f8b7cc502e867f7
miho1126 <iyq2loveeveryone@gmail.com>
2019-08-13 15:58:02 +0900

    追加と変更

--- app.js
+++ app.js
@@ -2,22 +2,51 @@ $(function(){
   $('.menuBtn').on('click',function(){
     // メニューに入るテキストを宣言
     let text;
-    
     // onのクラスを持っている時
-    if(!$(this).hasClass('on')){
-      text = 'close';
+    if($(this).hasClass('on')){
+      text = 'menu';
       // メニューをスライドダウン
-      $('.menu').slideDown();
+      $('#menu').slideUp();
       
     // onのクラスを持っていない時
     // 初期状態に戻す
-    } else {
-      text = 'menu';
+    } 
+    else {
+      text = 'close';
       // メニューをスライドアップして隠す
-      $('.menu').slideUp();
+      $('#menu').slideDown();
     }
     
     // ボタンにクラスonを付与
     $(this).toggleClass('on').text(text);
   });
+
+  console.log($('body,html').scrollTop());
+  $('.js-scroll').on('click',function(){
+    $('body,html').animate({scrollTop: 0 }, 500); 
+    console.log($(this).offset().top);
+    return false;
+  });
+
+  let swiper = new Swiper ('.swiper-container',{
+    loop: true,
+    slidesPerView: 1,
+    autoplay: {
+      delay: 3500,
+    },
+    pagination: {
+      el:'.swiper-pagination',
+      clickable:true,
+    },
+    // effect: 'cube',
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });
+
 });
+
